593f93910ebe86b207bf9aa8319197d8f86c57e3
an873sch <anna_schatz98@web.de>
2026-05-18 21:14:50 +0200

    Aufgabe 4 fertig

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-
-const repoName = '/frontend-pickandlook/'
 
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? repoName
-    : '/',
-
-  plugins: [
-    vue(),
-    vueDevTools()
-  ]
+  plugins: [vue()],
+  server: {
+    port: 5173
+  }
 })
